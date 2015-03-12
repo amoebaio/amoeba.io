c0fad682fcc69cc9f7f2a0f37e4cccd3266231c8
maxazan <maxazan@gmail.com>
2015-03-12 14:43:04 +0200

    0.0.12 del behaviors test add chaining-tool

--- build/amoeba.io.js
+++ build/amoeba.io.js
@@ -1,62 +1,62 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var Chain = require('chaining-tool');
+
+
 ClientHolder = function(client, use) {
     this.use = use;
     this.client = client;
-    this.behaviors_list = [];
+    this.handlers_list = {};
 };
 
-ClientHolder.prototype.behavior = function(type, func) {
-    if (!this.behaviors_list[type]) {
-        this.behaviors_list[type] = [];
+ClientHolder.prototype.handler = function(type, func) {
+    if (!this.handlers_list[type]) {
+        this.handlers_list[type] = [];
     }
-    this.behaviors_list[type].push(func);
+    this.handlers_list[type].push(func);
 };
 
-ClientHolder.prototype.behaviors = function(type) {
-    return this.behaviors_list[type];
+ClientHolder.prototype.handlers = function(type) {
+    return this.handlers_list[type] || [];
 };
 
 ClientHolder.prototype.invoke = function(method, params, callback) {
 
-    var bahaviors_before = this.behaviors("before_invoke");
-    var bahaviors_after = this.behaviors("after_invoke");
-
-    var data = {
-        use: this.use,
-        method: method,
-        params: params,
-        callback: callback
-    };
-
-    var counterBefore = -1;
-    var client = this.client;
-    var nextBefore = function() {
-        if (arguments.length === 0 || arguments.length === 1 && arguments[0] === true) {
-            counterBefore++;
-            if (bahaviors_before && bahaviors_before[counterBefore]) {
-                bahaviors_before[counterBefore](data, nextBefore);
-            } else {
-                client.invoke(data.use, data.method, data.params, function(err, result) {
-                    var counterAfter = -1;
-                    data.err = err;
-                    data.result = result;
-
-                    var nextAfter = function() {
-                        counterAfter++;
-                        if (bahaviors_after && bahaviors_after[counterAfter]) {
-                            bahaviors_after[counterAfter](data, nextAfter);
-                        } else {
-                            data.callback(data.err, data.result);
-                        }
-                    };
-                    nextAfter();
-                });
-            }
-        } else {
-            data.callback(data.err, data.result);
+    var context = {
+        request: {
+            use: this.use,
+            method: method,
+            params: params
+        },
+        response: {
+            error: null,
+            result: null
         }
     };
-    nextBefore();
+
+    var self = this;
+    //create before_invoke chain
+    var chain_bofore = new Chain(self.handlers("before_invoke"));
+    chain_bofore.start(context, function(context) {
+        //success before_invoke
+        self.client.invoke(context.request.use, context.request.method, context.request.params, function(error, result) {
+            //set result
+            context.response.error = error;
+            context.response.result = result;
+            //create after_invoke chain
+            var chain_after = new Chain(self.handlers("after_invoke"));
+            chain_after.start(context, function(context) {
+                //success after_invoke
+                callback(context.response.error, context.response.result);
+            }, function(context) {
+                //interrupted after_invoke
+                callback(context.response.error, context.response.result);
+            });
+        });
+    }, function(context) {
+        //interrupted before_invoke
+        callback(context.response.error, context.response.result);
+    });
+
 };
 
 ClientHolder.prototype.on = function(event, callback, onadded) {
@@ -69,7 +69,7 @@ ClientHolder.prototype.removeListener = function(event, listener, onremoved) {
 
 module.exports = exports = ClientHolder;
 
-},{}],2:[function(require,module,exports){
+},{"chaining-tool":3}],2:[function(require,module,exports){
 var ClientHolder = require('./amoeba-client-holder');
 
 Amoeba = function() {
@@ -102,4 +102,65 @@ Amoeba.prototype.server = function(server) {
 
 module.exports = exports = Amoeba;
 
-},{"./amoeba-client-holder":1}]},{},[1,2]);
+},{"./amoeba-client-holder":1}],3:[function(require,module,exports){
+module.exports = require('./lib/chain.js');
+},{"./lib/chain.js":4}],4:[function(require,module,exports){
+/**
+ * Main chain constructor
+ * @param {Array} handlers
+ */
+var Chain = function(handlers) {
+    //add handlers
+    this.handlers = handlers || [];
+    this.oncomplete = null;
+    this.onbreak = null;
+
+};
+
+/**
+ * Add handler to the end of chain
+ * @param {function} handler
+ */
+Chain.prototype.add = function(handler) {
+    this.handlers.push(handler);
+};
+
+/**
+ * Start execution
+ * @param  {Object} context
+ * @param  {Function} oncomplete
+ * @param  {Function} onbreak
+ * @return
+ */
+Chain.prototype.start = function(context, oncomplete, onbreak) {
+    this.oncomplete = oncomplete;
+    this.onbreak = onbreak;
+    this.next(0, context)();
+};
+
+/**
+ * Create function for passing execution to next handler
+ * @param  {Number}   i          index of handler
+ * @param  {Object}   context
+ * @return {Function} call this function you will pass execution to next handler
+ */
+Chain.prototype.next = function(i, context) {
+    var self = this;
+    return function() {
+        if (arguments.length === 0 || arguments.length === 1 && arguments[0] === true) {
+
+            if (self.handlers[i]) {
+                self.handlers[i](context, self.next(i + 1, context));
+            } else {
+                self.oncomplete(context);
+            }
+        } else {
+            if (self.onbreak) {
+                self.onbreak(context);
+            }
+        }
+    };
+};
+
+module.exports = exports = Chain;
+},{}]},{},[1,2]);
