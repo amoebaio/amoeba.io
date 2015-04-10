914c137df36327f5e816c080cb203bbdfdd4956c
maxazan <maxazan@gmail.com>
2015-04-10 14:45:13 +0300

    add docs

--- build/amoeba.io.js
+++ build/amoeba.io.js
@@ -3,6 +3,14 @@ var Chain = require('chaining-tool');
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 
+/**
+ * Creates a new Amoeba
+ * @class
+ * @author Maksym Pomazan
+ * @version 0.1.0
+ * @param {Amoeba} parent The parent amoeba
+ * @param {String} p      Path for current amoeba
+ */
 Amoeba = function(parent, p) {
     this._parent = parent || null;
     this._path = p || null;
@@ -12,6 +20,11 @@ Amoeba = function(parent, p) {
 
 util.inherits(Amoeba, EventEmitter);
 
+/**
+ * Method return root amoeba
+ *
+ * @return {Amoeba} The root amoeba
+ */
 Amoeba.prototype.root = function() {
     if (this._parent) {
         return this._parent.root();
@@ -20,10 +33,30 @@ Amoeba.prototype.root = function() {
     }
 };
 
-Amoeba.prototype.path = Amoeba.prototype.vacuole = function(p) {
+/**
+ * Method return new Amoeba accroding path
+ *
+ * @example
+ * var amoeba = new Amoeba();
+ * amoeba.path("chat").on("message", function(){});
+ * @param  {String} p Path
+ * @return {Amoeba}   new created amoeba
+ */
+Amoeba.prototype.path = function(p) {
     return new Amoeba(this.root(), p);
 };
 
+/**
+ * Method synonim for path method
+ */
+Amoeba.prototype.vacuole = Amoeba.prototype.path;
+
+/**
+ * Route amoeba path to Object
+ *
+ * @param {Object} Object for route
+ * @return {Amoeba} self for chain execution
+ */
 Amoeba.prototype.as = function() {
     if (this._path) {
         this.root()._vacuols[this._path] = arguments[0];
@@ -40,9 +73,15 @@ Amoeba.prototype.as = function() {
     } else {
         throw new Error("Path not found");
     }
-
 };
 
+/**
+ * Add event listener
+ *
+ * @param {String} event Event to listen. Can be mask.
+ * @param {Function} callback Callback function
+ * @return {Amoeba} self for chain execution
+ */
 Amoeba.prototype.on = function() {
     if (this._parent) {
         Array.prototype.unshift.call(arguments, this._path);
@@ -53,6 +92,13 @@ Amoeba.prototype.on = function() {
     return this;
 };
 
+/**
+ * Emit event
+ *
+ * @param {String} event Event to emit
+ * @param {Object} data Event data
+ * @return {Amoeba} self for chain execution
+ */
 Amoeba.prototype.emit = function() {
     var delimiter = ":";
     var path_delimiter = ".";
@@ -79,71 +125,13 @@ Amoeba.prototype.emit = function() {
     return this;
 };
 
-Amoeba.prototype.compare = function(mask, path) {
-    if (path == mask) {
-        return true;
-    }
-    var parts = path.split('.');
-    for (var i = parts.length; i >= 0; i--) {
-        var res = parts.slice(0, i);
-        res.push("*");
-        if (res.join(".") == mask) {
-            return true;
-        }
-    }
-    return false;
-};
-
-Amoeba.prototype.handle = Amoeba.prototype.use = function() {
-    if (this._parent) {
-        this.root().use(this._path, arguments[0], arguments[1]);
-    } else {
-        if (!this._handlers[arguments[1]]) {
-            this._handlers[arguments[1]] = [];
-        }
-        this._handlers[arguments[1]].push({
-            path: arguments[0],
-            handler: arguments[2]
-        });
-    }
-    return this;
-};
-
-Amoeba.prototype.handlers = function(type, path) {
-    if (this._parent) {
-        return this.root().handlers(type, path);
-    } else {
-        var res = [];
-        var all = this._handlers[type] || [];
-        for (var i = 0; i < all.length; i++) {
-            if (typeof(path) == "undefined" || this.compare(all[i].path, path)) {
-                res.push(all[i].handler);
-            }
-        }
-        return res;
-    }
-};
-
-Amoeba.prototype.route = function(path) {
-    if (this._parent) {
-        return this.root().route(path);
-    } else {
-        if (this._vacuols[path]) {
-            return this._vacuols[path];
-        }
-
-        var parts = path.split('.');
-        for (var i = parts.length; i >= 0; i--) {
-            var res = parts.slice(0, i);
-            res.push("*");
-            if (this._vacuols[res.join(".")]) {
-                return this._vacuols[res.join(".")];
-            }
-        }
-        return null;
-    }
-};
-
+/**
+ * Invoke method
+ * @param {String} method Method name
+ * @param {Object} [params] Object or Array
+ * @param {Function} [callback] Callback function for async methods
+ * @return {Amoeba} self for chain execution
+ */
 Amoeba.prototype.invoke = function() {
     if (this._parent) {
         Array.prototype.unshift.call(arguments, this._path);
@@ -207,7 +195,85 @@ Amoeba.prototype.invoke = function() {
         });
         return this;
     }
+};
+/**
+ * Add middleware logic
+ * @param {String} type Type of handler. before_invoke | after_invoke | before_event
+ * @param {Function} handler Handler
+ * @return {Amoeba} self for chain execution
+ */
+Amoeba.prototype.use = function() {
+    if (this._parent) {
+        this.root().use(this._path, arguments[0], arguments[1]);
+    } else {
+        if (!this._handlers[arguments[1]]) {
+            this._handlers[arguments[1]] = [];
+        }
+        this._handlers[arguments[1]].push({
+            path: arguments[0],
+            handler: arguments[2]
+        });
+    }
+    return this;
+};
 
+Amoeba.prototype.handle = Amoeba.prototype.use;
+
+/**
+ * Find handlers by type or by type and path
+ *
+ * @param  {String} type Type of handle
+ * @param  {String} path Used path
+ * @return {Array}      All founded handlers
+ */
+Amoeba.prototype.handlers = function(type, path) {
+    if (this._parent) {
+        return this.root().handlers(type, this._path);
+    } else {
+        var res = [];
+        var all = this._handlers[type] || [];
+        for (var i = 0; i < all.length; i++) {
+            if (typeof(path) == "undefined" || this.compare(all[i].path, path)) {
+                res.push(all[i].handler);
+            }
+        }
+        return res;
+    }
+};
+
+Amoeba.prototype.compare = function(mask, path) {
+    if (path == mask) {
+        return true;
+    }
+    var parts = path.split('.');
+    for (var i = parts.length; i >= 0; i--) {
+        var res = parts.slice(0, i);
+        res.push("*");
+        if (res.join(".") == mask) {
+            return true;
+        }
+    }
+    return false;
+};
+
+Amoeba.prototype.route = function(path) {
+    if (this._parent) {
+        return this.root().route(path);
+    } else {
+        if (this._vacuols[path]) {
+            return this._vacuols[path];
+        }
+
+        var parts = path.split('.');
+        for (var i = parts.length; i >= 0; i--) {
+            var res = parts.slice(0, i);
+            res.push("*");
+            if (this._vacuols[res.join(".")]) {
+                return this._vacuols[res.join(".")];
+            }
+        }
+        return null;
+    }
 };
 
 module.exports = exports = Amoeba;
@@ -644,6 +710,7 @@ process.browser = true;
 process.env = {};
 process.argv = [];
 process.version = ''; // empty string to avoid regexp issues
+process.versions = {};
 
 function noop() {}
 
--- readme.md
+++ readme.md
@@ -1,89 +1,10 @@
 #Amoeba
-
-##About
-
+Request/Responce/Event communication system
 
 ##Instalation
 
-```npm install amoeba.io```
-
-##Usage
-###Socket client-server application
-Install modules
 ```
-npm install socket.io amoeba.io amoeba.io-local-client amoeba.io-socket-server 
-```
-Create server
-```javascript
-//File server.js
-var ServerIO = require('socket.io');
-var Amoeba = require("amoeba.io");
-var LocalClient = require("amoeba.io-local-client");
-var SocketServer = require("amoeba.io-socket-server");
-//Service class
-Auth = function() {};
-
-Auth.prototype.login = function(data, callback) {
-    if (data.login == "admin" && data.password == "pass") {
-        callback(null, {
-            "res": "login ok"
-        });
-    } else {
-        callback({
-            "res": "login fail"
-        }, null);
-    }
-};
-
-//Create amoeba instance
-amoeba = new Amoeba();
-//Add service to amoeba
-amoeba.use("auth", new LocalClient(new Auth()));
-
-//Create socket server
-var port = "8090";
-io = new ServerIO();
-io.listen(port).on('connection', function(socket) {
-  //All services now you can use thru socket 
-  amoeba.server(new SocketServer(socket));
-});
-```
-Start server
-```
-node server
+npm install amoeba.io
 ```
 
-Create client
-```html
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Browser Amoeba test</title>
-    <meta charset="utf-8">
-
-    <link rel="stylesheet" href="http://code.jquery.com/qunit/qunit-1.17.1.css">
-
-    <script type="text/javascript" src="js/socket.io.js"></script>
-    <script type="text/javascript" src="js/amoeba.io.min.js"></script>
-    <script type="text/javascript" src="js/amoeba.io-socket-client.js"></script>
-    <script type="text/javascript">
-      var socket = io('http://localhost:8090');
-      var amoeba = new Amoeba();
-      socket.on('connect', function() {
-        amoeba.use("auth", new SocketClient(socket));
-        amoeba.use("auth").invoke("login", {
-                login: 'admin',
-                password: 'pass'
-            }, function(err, data) {
-              //Do somethong
-            });
-      }
-    </script>
-</head>
-
-<body>
-</body>
-
-</html>
-```
+Read more information at http://amoebaio.github.io/amoeba.io/
